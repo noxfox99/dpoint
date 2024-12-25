176192d1d17642bc2246bac39ea2348f78b7a1e0
Nox Fox <rickxxxmorty@proton.me>
2024-12-25 19:56:22 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import './index.css';
-
 const Header = () => (
   <header className="bg-black text-white py-4">
     <div className="container mx-auto flex justify-between items-center px-4">

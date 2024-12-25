01ff652dce1341b26812579901fc42a78dcb93da
Nox Fox <rickxxxmorty@proton.me>
2024-12-25 21:36:57 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'], // This scans all relevant files for classes
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React source files
+    "./public/index.html", // If Tailwind classes are used in your index.html
+  ],
   theme: {
     extend: {},
   },

1cad04a968030044ae71cbf02b8b4cdd15880d5c
Nox Fox <rickxxxmorty@proton.me>
2024-12-25 21:47:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
